c22a7ad4c5bed957dca1be8884f0b75f0c0918c2
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2025-12-22 10:00:03 +0300

    added password encryption

---
+++ ACME/src/PasswordEncryption.java
@@ -0,0 +1,16 @@
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class PasswordEncryption {
+    private String key = "kjh32423%#@$dafjk;dl;";
+    StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();;
+    public PasswordEncryption(){
+        this.enc.setPassword(key);
+        this.enc.setAlgorithm("PBEWithMD5AndDES");
+    }
+    public String encryptPassword(String plainPassword){
+        return enc.encrypt(plainPassword);
+    }
+    public String decryptPassword(String encryptedPassword){
+        return enc.decrypt(encryptedPassword);
+    }
+}
